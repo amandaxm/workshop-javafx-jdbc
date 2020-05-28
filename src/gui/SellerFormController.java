26843d337b6705bd042d6404086ee922e9c36991
Amanda Xavier Mariano <amandaxaviermariano10@gmail.com>
2020-05-28 18:55:37 -0300

    Seller Form

---
+++ src/gui/SellerFormController.java
@@ -0,0 +1,164 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exception.ValidationException;
+import model.services.SellerService;
+
+public class SellerFormController implements Initializable {
+
+	private Seller entity;
+
+	private SellerService service;
+
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();	@FXML
+	private TextField txtId;
+
+	@FXML
+	private TextField txtName;
+
+	@FXML
+	private Label lblError;
+
+	@FXML
+	private Button btSave;
+
+	@FXML
+	private Button btCancel;
+
+	public void setSeller(Seller entity) {
+		// instancia do departamento
+		this.entity = entity;
+	}
+
+	public void setSellerService(SellerService service) {
+		// instancia do departamento
+		this.service = service;
+	}
+
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+
+
+		dataChangeListeners.add(listener);
+
+
+	}
+
+	public void onBtSaveAction(ActionEvent event) {
+		if (entity == null) {
+			throw new IllegalStateException("Entidade esta null");
+
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service esta null");
+
+		}
+		try {
+			entity = getFormData();
+			// salvar no banco de dados
+			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();
+			Utils.currentStage(event).close();
+		} catch (ValidationException e) {
+
+			setErrorMessages(e.getErrors());
+
+		} catch (DbException e) {
+			Alerts.showAlert("Erro ao salvar obj", null, e.getMessage(), AlertType.ERROR);
+		}
+
+	}
+
+	private void notifyDataChangeListeners() {
+
+
+		for (DataChangeListener listener : dataChangeListeners) {
+
+
+			listener.onDataChanged();
+
+
+		}
+
+
+	}
+
+	private Seller getFormData() {
+		// pegar dados do forulario
+		Seller obj = new Seller();
+
+		ValidationException exception = new ValidationException("Validation error");
+
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+
+			exception.addError("name", "Nome nulo invalido");
+
+		}
+		obj.setName(txtName.getText());
+		// se tiver pelo menos um erro
+		if (exception.getErrors().size() > 0) {
+
+			throw exception;
+
+		}
+
+		return obj;
+	}
+
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
+
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtId, 30);
+
+	}
+
+	public void UpdateFormData() {
+		if (entity == null) {
+			throw new IllegalStateException("Entidade é null");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
+
+	}
+
+	private void setErrorMessages(Map<String, String> errors) {
+
+		Set<String> fields = errors.keySet();
+
+		if (fields.contains("name")) {
+
+			lblError.setText(errors.get("name"));
+
+		}
+
+	}
+
+}
--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -16,7 +17,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -24,6 +27,8 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
 import model.services.SellerService;
@@ -109,28 +114,28 @@ public class SellerListController implements Initializable, DataChangeListener {
 
 	public void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
 
-//		try {
-//			// carregar para preencher departamentp
-//			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-//			Pane pane = loader.load();
-//
-//			SellerFormController controller = loader.getController();
-//			controller.setSeller(obj);
-//			controller.setSellerService(new SellerService());
-//			controller.UpdateFormData();
-//			controller.subscribeDataChangeListener(this);
-//			Stage dialogStage = new Stage();
-//			dialogStage.setTitle("Entre com o departamento");
-//			dialogStage.setScene(new Scene(pane));
-//			dialogStage.setResizable(false);
-//			dialogStage.initOwner(parentStage);
-//			// tela travada
-//			dialogStage.initModality(Modality.WINDOW_MODAL);
-//			dialogStage.showAndWait();
-//		} catch (IOException e) {
-//			Alerts.showAlert("IO Exception", "Erro ao carregar tela", e.getMessage(), AlertType.ERROR);
-//
-//		}
+		try {
+			// carregar para preencher departamentp
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+
+			SellerFormController controller = loader.getController();
+			controller.setSeller(obj);
+			controller.setSellerService(new SellerService());
+			controller.UpdateFormData();
+			controller.subscribeDataChangeListener(this);
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Entre com os dados do vendedor");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			// tela travada
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Erro ao carregar tela", e.getMessage(), AlertType.ERROR);
+
+		}
 	}
 
 	@Override

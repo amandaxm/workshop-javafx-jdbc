a10f01e4165e997caf9e09b426352a7404cafe28
Amanda Xavier Mariano <amandaxaviermariano10@gmail.com>
2020-05-28 18:26:05 -0300

    SellerList

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import model.services.DepartmentService;
+import model.services.SellerService;
 
 public class MainViewController implements Initializable {
 
@@ -31,8 +32,12 @@ public class MainViewController implements Initializable {
 
 	@FXML // eventos
 	public void onMenuItemVendedorAction() {
-		System.out.println("onMenuVendedor");
-
+loadView("/gui/SellerList.fxml", (SellerListController controller)->{
+			
+			controller.setSellerService(new SellerService());
+			controller.updateTableView();	
+		
+		});
 	}
 
 	@FXML // eventos
@@ -42,11 +47,6 @@ public class MainViewController implements Initializable {
 			
 			controller.setDepartmentService(new DepartmentService());
 			controller.updateTableView();	
-		
-			
-			
-			
-			
 		});
 	}
 
